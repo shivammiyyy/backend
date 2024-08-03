5a98413290b7c3c49a0fafa3cd6189fb21b97058
Shivam Kumar <shivamprofessional1@gmail.com>
2024-08-04 01:36:57 +0530

    second commit

--- index.js
+++ index.js
@@ -3,8 +3,36 @@ import express from 'express'
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(express.static('dist'))
+
+// app.get('/', (req, res) => {
+  // res.send('Hello World!')
+// })
+
+app.get('/api/jokes',(req,res)=>{
+  const jokes = [
+    {
+      id : 1,
+      title : "joke1",
+      content : "this is joke"
+    },
+    {
+      id : 2,
+      title : "joke2",
+      content : "this is joke"
+    },
+    {
+      id : 1,
+      title : "joke2",
+      content : "this is joke"
+    },
+    {
+      id : 1,
+      title : "joke2",
+      content : "this is joke"
+    }
+  ]
+  res.send(jokes)
 })
 
 app.listen(process.env.PORT || port, () => {
